0231099bee7e505b1efc1e73f5e050f870337c98
X250 <aleksander.synoradzki@gmail.com>
2024-01-24 11:02:57 +0100

    project initialization

---
+++ paymove-recruitment/src/main/java/com/synoradzki/paymoverecruitment/PaymoveRecruitmentApplication.java
@@ -0,0 +1,13 @@
+package com.synoradzki.paymoverecruitment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymoveRecruitmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymoveRecruitmentApplication.class, args);
+    }
+
+}
---
+++ paymove-recruitment/src/test/java/com/synoradzki/paymoverecruitment/PaymoveRecruitmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.synoradzki.paymoverecruitment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymoveRecruitmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
